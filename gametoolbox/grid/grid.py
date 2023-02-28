ab0bba474a6de9d2efd76f768b32286bd65fb2ae
jbbranch9 <jacob.b.branch1@gmail.com>
2023-02-28 12:13:28 -0800

    developing Grid classes

--- gametoolbox/grid/grid.py
+++ gametoolbox/grid/grid.py
@@ -1,5 +1,12 @@
-from ..logic.engine_logic.operations import kwargs_to_vars
+"""
+a Grid is any matrix with 2 or more dimensions. variations include:
+Grid2D
+Grid3D
 
+related classes include:
+GridTimeDimension
+
+"""
 
 
 class Grid(list):
@@ -8,19 +15,74 @@ class Grid(list):
             *args,
             **kwargs,
     ):
+        super().__init__(*args, **kwargs)
+        self.timekeeper = self.GridTimeDimension(parent=self)
 
-        kwargs_to_vars(kwargs, self)
-        print(vars(self))
+    def record(self):
+        self_copy = list(self)
+        self.timekeeper.update_record(frame=self_copy)
 
-        print(vars())
+    """
+    GridTimeDimension is a list of all past states of a Grid object.
+    Future plans for this class:
+    1) delta recording mode, i.e. frames only track differences from previous frame, 
+       rather than copy the entire grid each frame (which is bad for memory)
+    2) offloading of oldest/most distant frames onto external files,
+       in order to save memory when dealing with particularly long timelines or large grids
+    """
+    class GridTimeDimension(list):
+        def __init__(self, parent, auto_start_recording: bool = False):
+            super().__init__([])
+            self.__parent = parent
+            self.__recording = auto_start_recording
 
-        super().__init__([])
+        def start_recording(self):
+            self.__recording = True
+
+        def stop_recording(self):
+            self.__recording = False
+
+        def update_record(self, frame):
+            if self.__recording:
+                self.append(frame)
+            else:
+                raise Exception("This grid's timekeeper is not set to record.")
+
+
+class Grid2D(Grid):
+    __DEFAULT_x = 8
+    __DEFAULT_y = 8
+
+    def __init__(
+            self,
+            x_dimension: int = __DEFAULT_x,
+            y_dimension: int = __DEFAULT_y,
+            container_type=list,
+    ):
+
+        def empty_cell():
+            container_type([])
+
+        super().__init__(
+            [[empty_cell() for _column in range(x_dimension)] for _row in range(y_dimension)]
+        )
+
+    def __str__(self):
+        output = ""
+        for row in self:
+            output += str(row)+"\n"
+        return output
+
 
 def main():
-    g = Grid(dimensions= (1,1),
-             init_list=None,
-             extra_param=2)
+    g = Grid2D(
+        x_dimension=3,
+        y_dimension=4,
+        container_type=tuple
 
-    print(vars(g))
+    )
+    print(g)
+
+
 if __name__ == "__main__":
     main()
