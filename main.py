02357d8f25fe8abe1d041c015d25b8eb37d63ed2
jbbranch9 <jacob.b.branch1@gmail.com>
2023-03-02 23:17:12 -0800

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from gametoolbox.matrix import matrix
+from gametoolbox.matrix.matrix import Matrix2D
+from gametoolbox.maze.draw_maze import DrawMaze
 
 def main():
-    matrix.main()
+    mtx = Matrix2D(x_dimension=3, y_dimension=4)
+    print(iter(mtx))
+
+
+
 if __name__ == "__main__":
     main()
