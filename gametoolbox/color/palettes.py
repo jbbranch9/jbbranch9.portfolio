3f8ec6d9108f62ed5e99f855178121f5cbac6023
jbbranch9 <jacob.b.branch1@gmail.com>
2023-03-07 20:39:10 -0800

    added color palettes

---
+++ gametoolbox/color/palettes.py
@@ -0,0 +1,27 @@
+
+colors = {
+    "white": "#FFFFFF",
+    "black": "#000000",
+
+    "gray_timberwolf": "#d4d4d4",
+    "green_brunswick": "#395949",
+}
+
+palettes = {
+    "board": {
+        "chess": {
+            "black-white": {
+                "light_color": colors["white"],
+                "dark_color": colors["black"],
+                },
+            "black-white-soft": {
+                "light_color": "#cccccc",
+                "dark_color": "#333333",
+                },
+            "green-white": {
+                "light_color": colors["gray_timberwolf"],
+                "dark_color": colors["green_brunswick"],
+                },
+        },
+    },
+}
