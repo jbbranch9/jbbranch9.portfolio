41051a29b0b3c3bddd702fbd1959c5173ce7a97d
jbbranch9 <jacob.b.branch1@gmail.com>
2023-03-10 20:57:43 -0800

    MazeRoom WIP

--- gametoolbox/maze/maze_room.py
+++ gametoolbox/maze/maze_room.py
@@ -1,8 +1,60 @@
+from maze_room_sections import \
+    RightWall, RightCeilingJoint, RightFloorJoint, \
+    LeftWall, LeftCeilingJoint, LeftFloorJoint, \
+    Ceiling, CenterWall, Floor
 
 
-class MazeRoom:
+class MazeRoom(list):
+    def __init__(
+            self,
+            left_state: str,
+            center_state: str,
+            right_state: str,
+            ceiling_state: str,
+            floor_state: str,
+    ):
 
-    side_wall_len = 25
-    back_wall_len = 29
+        self.left_ceiling = LeftCeilingJoint(state=left_state)
+        self.left = LeftWall(state=left_state)
+        self.left_floor = LeftFloorJoint(state=left_state)
+
+        self.ceiling = Ceiling(state=ceiling_state)
+        self.center = CenterWall(state=center_state)
+        self.floor = Floor(state=floor_state)
+
+        self.right_ceiling = RightCeilingJoint(state=right_state)
+        self.right = RightWall(state=right_state)
+        self.right_floor = RightFloorJoint(state=right_state)
+
+        left_side = self.left_ceiling + self.left + self.left_floor
+        center = self.ceiling + self.center + self.floor
+        right_side = self.right_ceiling + self.right + self.right_floor
+
+        zipped = list(zip(left_side, center, right_side))
+
+        super().__init__([row[0]+row[1]+row[2]+"\n" for row in zipped])
+
+    def __str__(self):
+        output = ""
+        for row in self:
+            output += row
+            output += "\n"
+        return output
+
+def main():
+    mr = MazeRoom(
+        left_state="open",
+        center_state="closed",
+        right_state="closed",
+        ceiling_state="open",
+        floor_state="open",
+    )
 
 
+    with open("test.txt", mode="w", encoding="utf-8") as file:
+        file.writelines(mr)
+        file.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ gametoolbox/maze/maze_room_sections.py
@@ -0,0 +1,155 @@
+from gametoolbox.os.file_handling import clip_from_file
+
+
+class MazeRoomSection(list):
+
+    def __init__(
+            self,
+            horizontal_range: tuple[int, int] = None,
+            vertical_range: tuple[int, int] = None,
+            room: str = "main",
+            state: str = "open",
+    ):
+
+        assert room in ("main", "next")
+        assert state in ("open", "closed", "open_open", "open_closed", "open_hidden")
+
+        filename = f"maze_room_reference/mazeroom_{room}_{state}.txt"
+
+        x_min, x_max = horizontal_range
+        y_min, y_max = vertical_range
+
+        clip = clip_from_file(
+            filename=filename,
+            x_start=x_min,
+            x_end=x_max,
+            y_start=y_min,
+            y_end=y_max,
+        )
+
+        super().__init__([x.rstrip("\n") for x in clip])
+
+    def print_all(self):
+        print(*self, sep="\n")
+
+    def get_range(self, range_name: str):
+        left_range = (0, 25)
+        center_range = (25, 54)
+        right_range = (54, 80)
+
+        ceiling_range = (0, 13)
+        wall_range = (13, 25)
+        floor_range = (25, 36)
+
+        assert range_name in ("left", "center", "right", "ceiling", "wall", "floor")
+        return vars()[f"{range_name}_range"]
+
+
+class Ceiling(MazeRoomSection):
+    def __init__(self, state: str):
+        get_range = super().get_range
+        super().__init__(
+            horizontal_range=get_range("center"),
+            vertical_range=get_range("ceiling"),
+            room="main",
+            state=state,
+        )
+
+
+class Floor(MazeRoomSection):
+    def __init__(self, state: str):
+        get_range = super().get_range
+        super().__init__(
+            horizontal_range=get_range("center"),
+            vertical_range=get_range("floor"),
+            room="main",
+            state=state,
+        )
+
+
+class CenterWall(MazeRoomSection):
+    def __init__(self, state: str):
+        get_range = super().get_range
+        super().__init__(
+            horizontal_range=get_range("center"),
+            vertical_range=get_range("wall"),
+            room="main",
+            state=state,
+        )
+
+
+class LeftWall(MazeRoomSection):
+    def __init__(self, state: str):
+        get_range = super().get_range
+        super().__init__(
+            horizontal_range=get_range("left"),
+            vertical_range=get_range("wall"),
+            room="main",
+            state=state,
+        )
+
+
+class RightWall(MazeRoomSection):
+    def __init__(self, state: str):
+        get_range = super().get_range
+        super().__init__(
+            horizontal_range=get_range("right"),
+            vertical_range=get_range("wall"),
+            room="main",
+            state=state,
+        )
+
+
+class RightCeilingJoint(MazeRoomSection):
+    def __init__(self, state: str):
+        get_range = super().get_range
+        super().__init__(
+            horizontal_range=get_range("right"),
+            vertical_range=get_range("ceiling"),
+            room="main",
+            state=state,
+        )
+
+
+class LeftCeilingJoint(MazeRoomSection):
+    def __init__(self, state: str):
+        get_range = super().get_range
+        super().__init__(
+            horizontal_range=get_range("left"),
+            vertical_range=get_range("ceiling"),
+            room="main",
+            state=state,
+        )
+
+
+class RightFloorJoint(MazeRoomSection):
+    def __init__(self, state: str):
+        get_range = super().get_range
+        super().__init__(
+            horizontal_range=get_range("right"),
+            vertical_range=get_range("floor"),
+            room="main",
+            state=state,
+        )
+
+
+class LeftFloorJoint(MazeRoomSection):
+    def __init__(self, state: str):
+        get_range = super().get_range
+        super().__init__(
+            horizontal_range=get_range("left"),
+            vertical_range=get_range("floor"),
+            room="main",
+            state=state,
+        )
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+
