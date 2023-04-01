05237b970eec1da2c0f0fa1f1fbb35628c8325e2
jbbranch9 <jacob.b.branch1@gmail.com>
2023-03-31 22:14:17 -0700

    added safe_write and save functions

--- gametoolbox/os/file_handling.py
+++ gametoolbox/os/file_handling.py
@@ -1,4 +1,36 @@
 import base64
+import logging
+
+
+def safe_write(
+        data: str,
+        file_name: str,
+        directory: str,
+        file_extension: str = ".txt") -> bool:
+    try:
+        with open(f"{directory}/{file_name}.{file_extension}", "w", encoding='utf-8',) as file:
+            file.write(data)
+            file.close()
+        return True
+    except FileExistsError:
+        logging.exception(msg="FileExistsError.\nSave failed.")
+        return False
+
+
+def save(
+        data: str,
+        file_name: str,
+        folder_name: str,
+        file_extension: str) -> bool:
+    directory = "gametoolbox/save_data/" + folder_name
+    return safe_write(data=data, file_name=file_name, directory=directory, file_extension=file_extension)
+
+
+def save_level(
+        data: str,
+        level_name: str,
+        folder_name: str) -> bool:
+    return save(data=data, file_name=level_name, folder_name=f"levels/{folder_name}", file_extension="lvl")
 
 
 def base64_string(filename: str) -> str:
@@ -9,7 +41,7 @@ def base64_string(filename: str) -> str:
     return b64_string
 
 
-def clip_from_file(
+def clip_from_text_file(
         filename: str,
         x_start: int,
         x_end: int,
