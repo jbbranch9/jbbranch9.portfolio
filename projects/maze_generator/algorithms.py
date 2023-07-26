7a3bcc6564d2d26eb8c8df11f7a2dc6b305a404c
jbbranch9 <jacob.b.branch1@gmail.com>
2023-07-25 19:51:04 -0700

    Update algorithms.py

--- projects/maze_generator/algorithms.py
+++ projects/maze_generator/algorithms.py
@@ -1,58 +1,58 @@
-import random 
+import random
 
 from grid import MazeGrid
+
+
+def coin_toss():
+    return bool(random.randint(0,1))
 
 def blobby(maze: MazeGrid):
     maze.close_outer_walls()
     cells = list(maze.cells.values())
-
-    A_cells = []
-    B_cells = []
-
-    def mark_A(cell):
-        x, y = cell.x, cell.y
-        maze.update_cell(x, y, fill=0.25)
-
-    def mark_B(cell):
-        x, y = cell.x, cell.y
-        maze.update_cell(x, y, fill=0.75)
-
-    def select_and_remove_random_cell():
-        c = random.choice(cells)
-        cells.remove(c)
-        return c
-
-    cell_marker_function = {
-        "A": mark_A,
-        "B": mark_B,
-    }
-
-    def add_cell_to_group(cell_group, cell=None):
-        if not cell:
-            cell = select_and_remove_random_cell()
-        vars(f"{cell_group}_cells").append(cell)
-        cell_marker_function[cell_group](cell)
-
-
-
-
-    add_cell_to_group("A")
-
-    r = random.choice(A_cells)
-    print(r.x, r.y)
         
     print(maze)
-    print(A_cells)
-    print(B_cells)
-    print(cells)
         
-        
+
+def binary(maze: MazeGrid):
     
+    num_cols, num_rows = maze.grid_dimensions
+    col_ix_range = range(1, num_cols+1)
+    row_ix_range = range(1, num_rows+1)
+    
+    maze.close_all_walls()
+
+    for y in row_ix_range:
+        columns = list(col_ix_range)
+
+        run = []
+
+        while columns:
+            x = columns.pop(0)
+            
+            cut_right = coin_toss() and len(columns) > 0
+                
+            if cut_right or len(run) == 0:
+                run.append((x, y))
+                
+            if cut_right or y == row_ix_range[-1]:
+                maze.update_cell(x=x, y=y, right_wall=False)
+            else:
+                down_cut_x, down_cut_y = random.choice(run)
+                run = []
+                if y != row_ix_range[-1]:
+                    maze.update_cell(x=down_cut_x, y=down_cut_y, down_wall=False)
+                
+            
+
+            print(maze)
+    
+    maze.update_cell(1,1, left_wall=False)
     
 def main():
-    m = MazeGrid(10, 7)
-    blobby(m)
-    m.publish()
+    m = MazeGrid(15, 15)
+    binary(m)
+    print(m)
+
 
 if __name__ == "__main__":
     main()
